6069c85084e43060dec599b365eec6828b8e1959
Nianyi Chen <nianyic@login4.frontera.tacc.utexas.edu>
2023-12-22 19:25:28 -0600

    migrated compression job script to here

---
+++ compress_snapshot/job_scripts/PART_files/job_029.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+#SBATCH -p development
+#SBATCH --time=2:00:00
+#SBATCH --nodes=20
+##SBATCH --mem=200GB
+#SBATCH --ntasks-per-node=28
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nianyic@andrew.cmu.edu
+#SBATCH --job-name=comp-029
+#SBATCH --output=%x.out
+
+module load impi/19.0.9
+export I_MPI_HYDRA_TOPOLIB=ipl
+module unload python3
+source activate fast-mpi4py
+
+dataroot="$HOME/scratch3/Astrid/PART_temp"
+
+
+snap=029
+ifile="$dataroot/PART_$snap"
+ofile="$dataroot/PART_$snap-compressed"
+codedir="$HOME/AstridData/compress_snapshot"
+
+ibrun -n 560 python3 $codedir/fullsnap_compress.py --ifile $ifile --tol 0.03 --ofile $ofile --nfiles 256 --nloops 100
+
+wait
+echo "Done with compressing"
+
+cp -r $ifile/Header $ofile
+cp -r $ifile/Neutrino $ofile
+
+echo "copied over header and neutrino"
+date
---
+++ compress_snapshot/job_scripts/PIG_files/job_499.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+#SBATCH -p development
+#SBATCH --time=2:00:00
+#SBATCH --nodes=20
+##SBATCH --mem=200GB
+#SBATCH --ntasks-per-node=28
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nianyic@andrew.cmu.edu
+#SBATCH --job-name=comp-499
+#SBATCH --output=%x.out
+
+module load impi/19.0.9
+export I_MPI_HYDRA_TOPOLIB=ipl
+module unload python3
+source activate fast-mpi4py
+
+outroot="$HOME/scratch3/Astrid/PIG_compressed"
+dataroot="$HOME/ASTRID2_PIG"
+
+
+snap=499
+ifile="$dataroot/PIG_$snap"
+ofile="$outroot/PIG_$snap-compressed"
+codedir="$HOME/AstridData/compress_snapshot"
+
+ibrun -n 560 python3 $codedir/fullsnap_compress.py --ifile $ifile --tol 0.03 --ofile $ofile --nfiles 256 --nloops 100
+
+wait
+echo "Done with compressing"
+
+cp -r $ifile/Header $ofile
+cp -r $ifile/FOFGroups $ofile
+
+echo "copied over header and FOF"
+date
