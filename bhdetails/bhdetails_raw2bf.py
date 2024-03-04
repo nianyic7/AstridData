39aeed851f43c751c827c6a5348f1fcc2f133b4a
nianyic7 <nianyi.chen7@gmail.com>
2024-03-04 14:57:19 -0500

    add bhdetails processing

--- bhdetails/bhdetails_raw2bf.py
+++ bhdetails/bhdetails_raw2bf.py
@@ -4,57 +4,97 @@ import struct
 import glob
 import os, sys
 from mpi4py import MPI
+from bigfile import FileMPI
+import argparse
+
+SEL_COL=['acBHMass', 'BHMass', 'Density', 'KEflag',  'Mdyn', 'NumDM', 'V1sumDM',
+'acMass', 'BHpos', 'Encounter', 'KineticFdbkEnergy','MgasEnc', 'Swallowed', 'V2sumDM',
+'BHID', 'BHvel', 'Entropy', 'DFAccel','DragAccel','GravAccel', 'Mdot', 'Mtrack', 'SwallowID',  'z']
 
 
+class BHType:
+    def __init__(self, name_sel=None):
+        self.name_all = ('size','BHID','BHMass','Mdot','Density','timebin','Encounter','MinPos',\
+             'MinPot','Entropy','GasVel','acMom','acMass','acBHMass',\
+             'Fdbk','SPHID','SwallowID','CountProgs','Swallowed',\
+             'BHpos','srDensity','srParticles','srVel','srDisp',\
+             'DFAccel','DragAccel','GravAccel','BHvel','Mtrack','Mdyn',\
+             'KineticFdbkEnergy','NumDM','V1sumDM','V2sumDM','MgasEnc','KEflag','z','size2')
+        self.dtype_all = ('i','q','d','d','d','i','i','3d',\
+            'd','d','3d','3d','d','d',\
+            'd','q','q','i','i',\
+            '3d','d','d','3d','d',\
+            '3d','3d','3d','3d','d','d',\
+            'd','d','3d','d','d','i','d','i')
+
+        if name_sel is None:
+            name_sel = self.name_all
+        self.name_sel = name_sel
+        self.name2type = {name: dtype for name, dtype in zip(self.name_all, self.dtype_all)}
+
+
+    @property
+    def TypeAll(self):
+        np_type = np.dtype({'names':self.name_all, 'formats':self.dtype_all})
+        return np_type
+    @property
+    def TypeSel(self):
+        name_sel = [name for name in self.name_all if name in name_sel]
+        dtype_sel = [dtype for name, dtype in zip(self.name_all, self.dtype_all) if name in name_sel]
+        np_type = np.dtype({'names':name_sel, 'formats':dtype_sel})
+        return np_type
 
 
 def get_binary_files(ifile):
     filelist = []
     for f in glob.glob(ifile + "/*"):
         filelist.append(f)
-    print("Found %d binary files" % len(filelist))
     return filelist
+
+
+def get_length_offset(flist):
+    Length = []
+    Offset = []
+    offset = 0
+    data = np.zeros(1, dtype=BHTYPE.TypeAll)
+    nsingle = data.nbytes
+    print(f"Size of single element: {nsingle} bytes")
+
+    for ff in flist[:]:
+        file_stats = os.stat(ff)
+        Length.append(file_stats.st_size // nsingle)
+        Offset.append(offset)
+        offset += Length[-1]
+    return Length, Offset
 
 
         
 def read_binary_file(file):
-    fields = ('size','BHID','BHMass','Mdot','Density','timebin','encounter','MinPos',\
-             'MinPot','Entropy','GasVel','acMom','acMass','acBHMass',\
-             'Fdbk','SPHID','SwallowID','CountProgs','Swallowed',\
-             'BHpos','srDensity','srParticles','srVel','srDisp',\
-             'DFAccel','DragAccel','GravAccel','BHvel','Mtrack','Mdyn',\
-             'KinFdbk','NumDM','V1sumDM','V2SumDM','MgasEnc','KEflag','z','size2')
-    dtype = ('i','q','d','d','d','i','i','3d',\
-            'd','d','3d','3d','d','d',\
-            'd','q','q','i','i',\
-            '3d','d','d','3d','d',\
-            '3d','3d','3d','3d','d','d',\
-            'd','d','3d','d','d','i','d','i')
-    dt = {'names':fields, 'formats':dtype}
+    dt = BHTYPE.TypeAll
+    dtype = BHTYPE.dtype_all
     #-----------------------------------------------------------
     struct_fmt = ''.join(list(dtype))
     struct_len = struct.calcsize(struct_fmt)
-    results = []
-    for filename in bhfile_zoo[istart:iend]:
-        data = np.fromfile(filename, dtype=dt, count=-1)
-        results.append(data)
-    results = np.concatenate(results)
+    results = np.fromfile(file, dtype=dt, count=-1)
 
     results['z'] = 1./results['z'] - 1
-    print("rank %d: read %d BHs from %d files"%(rank, len(results), len(bhfile_zoo[istart:iend])))
+    print("rank %d: read %d BHs"%(rank, len(results)))
+    assert len(results) == Length[i], "number of BHs read in is not consistent with pre-determined size!"
     print(f"Total memory occupied: {results.nbytes/1e9} GBs")
     return results
 
 
+def init_block(bf_w, blocknames, nfile):
+    block_dict = {}
+    for blockname in blocknames:
+        dtype = BHTYPE.name2type[blockname]
+        dsize = NTOT
+        block = bf_w.create(blockname, dtype, dsize, nfile)
+        block_dict[blockname] = block
 
-
-
-
-
-
-
-
-
+        if rank == 0:
+            print("Initizlized block:", blockname, flush=True)
+    return block_dict
 
 if __name__ == "__main__":
 
@@ -75,81 +115,47 @@ if __name__ == "__main__":
     parser.add_argument(
         "--nfiles", required=True, type=int, help="number of files in the output snap"
     )
-    parser.add_argument(
-        "--nloops", required=True, type=int, help="number of loops per task"
-    )
-    parser.add_argument(
-        "--blocknames", nargs="+", help="blocks to save", default=None
-    )
+    # parser.add_argument(
+    #     "--blocknames", nargs="+", help="blocks to save", default=None
+    # )
 
     args = parser.parse_args()
     # ----------- MPI Init ----------------
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-
-    # --------------------------
-    filelist = get_binary_files(args.ifile)
-    Nraw = len(filelist)
-    istart = Nraw * rank // size
-    iend = Nraw * (rank + 1) // size
-    print("Rank %03d will process file %03d to file %03d" % (rank, istart, iend), flush=True)
-
-
-
-   
-
-
-
-
-
-
-
-
     bf_w = FileMPI(comm, args.ofile, create=True)
     nfiles = int(args.nfiles)
-    nloops = int(args.nloops)
+    
 
-    # block names to process
-    blocknames = []
-    if args.blocknames is None:
-        all_blocks = sorted(glob.glob(args.ifile + "/*/*"))
-        for ff in all_blocks:
-            column = ff.split("/")[-1]
-            ptype = ff.split("/")[-2]
-            blockname = ptype + "/" + column
-
-            if ptype in ["0", "1", "4", "5"]:
-                blocknames.append(blockname)
-    else:
-        for bn in args.blocknames:
-            if os.path.isdir(args.ifile + "/" + bn):
-                blocknames.append(bn)
-            elif rank == 0:
-                print("Block %s does not exit!" % (bn), flush=True)
-
-    comm.barrier()
-
+    # --------------------------
+    BHTYPE = BHType(SEL_COL)
+    filelist = get_binary_files(args.ifile)
+    Nraw = len(filelist)
     if rank == 0:
-        print("All blocks to process:", flush=True)
-        for bb in blocknames:
-            print(bb, flush=True)
+        print(f"Total number of files: {Nraw}", flush=True)
 
+    # get Nperfile
+    Length, Offset = get_length_offset(filelist)
+    NTOT = sum(Length)
     comm.barrier()
     if rank == 0:
-        t1 = time.time()
-    for blockname in blocknames:
-        oblock, Ntot = init_block(bf_r, bf_w, blockname, nfiles)
-        comm.barrier()
-        rewrite_chunk(bf_r, bf_w, rank, Ntot, size, blockname, oblock)
-        comm.barrier()
+        print(f"Total number of data: {NTOT}", flush=True)
+    # initialize bf
+    block_dict = init_block(bf_w, blocknames=SEL_COL, nfile=nfiles)
+    comm.barrier()
 
-        if rank == 0:
-            t2 = time.time()
-            print("Finished Processing {: <15}".format(blockname), flush=True)
-            print("time used = %.1f min:" % ((t2 - t1) / 60.0), flush=True)
-            t1 = t2
-        comm.barrier()
-
+    # split task and io
+    fstart = Nraw * rank // size
+    fend = Nraw * (rank + 1) // size
+    print("Rank %03d will process file %03d to file %03d" % (rank, fstart, fend), flush=True)
+    for i in range(fstart, fend):
+        file = filelist[i]
+        data = read_binary_file(file)
+        for blockname in SEL_COL:
+            block = block_dict[blockname]
+            block.write(Offset[i], data[blockname])
+        print("Rank %03d finished processing file %03d" % (rank, i), flush=True)
+    comm.barrier()
     if rank == 0:
         print("Done!", flush=True)
--- bhdetails/test.py
+++ bhdetails/test.py
@@ -1,4 +1,4 @@
-import os, glob
+import os, glob,struct
 import numpy as np
 fields = ('size','BHID','BHMass','Mdot','Density','timebin','encounter','MinPos',\
          'MinPot','Entropy','GasVel','acMom','acMass','acBHMass',\
@@ -18,12 +18,38 @@ data = np.zeros(1, dtype=dt)
 nsingle = data.nbytes
 
 
-file_name = "/home1/08942/nianyic/ASTRID2_PIG/BlackholeDetails-R600"
+def read_binary_file(file):
+    fields = ('size','BHID','BHMass','Mdot','Density','timebin','encounter','MinPos',\
+             'MinPot','Entropy','GasVel','acMom','acMass','acBHMass',\
+             'Fdbk','SPHID','SwallowID','CountProgs','Swallowed',\
+             'BHpos','srDensity','srParticles','srVel','srDisp',\
+             'DFAccel','DragAccel','GravAccel','BHvel','Mtrack','Mdyn',\
+             'KinFdbk','NumDM','V1sumDM','V2SumDM','MgasEnc','KEflag','z','size2')
+    dtype = ('i','q','d','d','d','i','i','3d',\
+            'd','d','3d','3d','d','d',\
+            'd','q','q','i','i',\
+            '3d','d','d','3d','d',\
+            '3d','3d','3d','3d','d','d',\
+            'd','d','3d','d','d','i','d','i')
+    dt = {'names':fields, 'formats':dtype}
+    #-----------------------------------------------------------
+    struct_fmt = ''.join(list(dtype))
+    struct_len = struct.calcsize(struct_fmt)
+    data = np.fromfile(file, dtype=dt, count=-1)
+    results = data
+
+    results['z'] = 1./results['z'] - 1
+    print("read %d BHs"%(len(results)))
+    print(f"Total memory occupied: {results.nbytes/1e9} GBs")
+    return results
+
+file_name = "/hildafs/datasets/Asterix/BH_details_bigfile2/test"
 flist = sorted(glob.glob(file_name + "/*"))
 
 for ff in flist[:20]:
-	file_stats = os.stat(ff)
-	print(ff)
-	print(file_stats)
-	print(f"File Size in Bytes is {file_stats.st_size}")
-	print(f"number of element {file_stats.st_size / nsingle}")
+    file_stats = os.stat(ff)
+    print(ff)
+    print(file_stats)
+    print(f"File Size in Bytes is {file_stats.st_size}")
+    print(f"number of element {file_stats.st_size / nsingle}")
+    read_binary_file(ff)
