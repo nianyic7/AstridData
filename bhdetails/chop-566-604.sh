a8f338b3fb911930cc632d99950d0a4c5f9d10c2
Nianyi Chen <nianyi.chen7@gmail.com>
2024-03-06 21:40:36 -0500

    update merger jobs

--- bhdetails/chop-566-604.sh
+++ bhdetails/chop-566-604.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-#SBATCH --nodes 1
-#SBATCH -n 64
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
 #SBATCH --time 4:00:00
-#SBATCH --job-name chop-566-604
-#SBATCH --partition RM-shared
+#SBATCH --job-name chop-566-604-redo
+#SBATCH --partition RM
 #SBATCH -o slurm-%x.out
-#SBATCH --mem=110G
+#SBATCH --mem=200G
 
 source activate default
 codedir="$HOME/AstridData/bhdetails"
 
-for snap in 566 572 577 582 583 586 592 594 599 600 604
+#for snap in 566 572 577 582 583 586 592 594 599 600 604
+#do
+#ifile="$HOME/scratch1/Astrid/bhdetails/BH-Details-R$snap"
+#ofile="$HOME/scratch1/Astrid/bhdetails-chopped/BH-Details-R$snap"
+#srun python3 $codedir/bhdetails_chop.py --ifile $ifile --ofile $ofile --nfiles 8 --nchunks 6
+#done
+
+for snap in 566 572 586 594 600
 do
+echo "processing $snap"
 ifile="$HOME/scratch1/Astrid/bhdetails/BH-Details-R$snap"
 ofile="$HOME/scratch1/Astrid/bhdetails-chopped/BH-Details-R$snap"
 srun python3 $codedir/bhdetails_chop.py --ifile $ifile --ofile $ofile --nfiles 8 --nchunks 6
 done
-
--- merger_catalog/example_scripts/raw_merger_br.sh
+++ merger_catalog/example_scripts/raw_merger_br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes 1
 #SBATCH -n 1
-#SBATCH --time 10:00:00
+#SBATCH --time 6:00:00
 #SBATCH --job-name mraw-052
 #SBATCH --partition RM
 #SBATCH -o slurm-%x.out
@@ -11,5 +11,5 @@ source activate default
 codedir="$HOME/AstridData/merger_catalog/codes/raw_merger"
 snap="052"
 
-srun python3 $codedir/extract-merger-v2.py "vera" $snap 
+srun python3 $codedir/extract-merger-v2.py "bridges" $snap 
 
